515680e3af3c1d5b3e6501e7f5d40ba1b73be8e9
CrispStrobe <154636388+CrispStrobe@users.noreply.github.com>
2024-05-30 15:37:01 +0200

    Update langchain_util.py

--- src/langchain_util.py
+++ src/langchain_util.py
@@ -1,14 +1,13 @@
 import os
-
 import tiktoken
+from langchain_openai import ChatOpenAI
 
 enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
-
 
 def num_tokens_by_tiktoken(text: str):
     return len(enc.encode(text))
 
-
+def init_langchain_model(llm: str, model_name: str, temperature: float = 0.0, max_retries=5, timeout=60, **kwargs):
     """
     Initialize a language model from the langchain library.
     :param llm: The LLM to use, e.g., 'openai', 'together'
@@ -35,14 +34,17 @@ def num_tokens_by_tiktoken(text: str):
         except Exception as e:
             print(f"Error initializing ChatOpenAI: {e}\n")
             return None
+
     elif llm == 'together':
         # https://python.langchain.com/v0.1/docs/integrations/chat/together/
         from langchain_together import ChatTogether
         return ChatTogether(api_key=os.environ.get("TOGETHER_API_KEY"), model=model_name, temperature=temperature, **kwargs)
+    
     elif llm == 'ollama':
         # https://python.langchain.com/v0.1/docs/integrations/chat/ollama/
         from langchain_community.chat_models import ChatOllama
         return ChatOllama(model=model_name)  # e.g., 'llama3'
+    
     else:
         # add any LLMs you want to use here using LangChain
         raise NotImplementedError(f"LLM '{llm}' not implemented yet.")
